1a4da4cf3cc2f23b7b863ed935ec565487d850cb
Kevin Chen <kchen01@stuy.edu>
2018-11-25 19:29:59 -0500

    added a lot of things, not sure if they work

--- MyString.java
+++ MyString.java
@@ -1,13 +1,35 @@
 public class MyString implements CharSequence,Comparable<CharSequence>{
+  // data is the only private variable;
   private char[] data;
+  String arrayerror = "Will throw Excpetion if start or end are negative, if end is greater than length(), or if start is greater than end";
   public MyString(CharSequence s){
   }
   public CharSequence subSequence(int start, int end) {
-    if (start < 0 || end < 0 || end > data.length) {throw new IndexOutOfBoundsExecption("Will throw Excpetion if start or end are negative, if end is greater than length(), or if start is greater than end");}
+    if (start < 0 || end < 0 || end > data.length) {throw new IndexOutOfBoundsException(arrayerror);}
+    CharSequence input = this.subSequence(start, end);
+    MyString output = new MyString(input);
+    return output;
   }
-  String output = s;
-  while (end > start) {
-    s += data[start];
+  public char charAt(int index) {
+    if (index < 0 || index > data.length) {throw new IndexOutOfBoundsException(arrayerror);}
+    return data[index];
   }
-  return ouput;
+
+  public int length() {
+    return data.length;
+  }
+  public int compareTo(CharSequence other) {
+    for (int i = 0; i < this.length() && i < other.length(); i += 1) {
+      if (this.charAt(i) < other.charAt(i)) {
+        return -1;
+      }
+      if (this.charAt(i) > other.charAt(i)) {
+        return 1;
+      }
+    }
+    if (this.length() == other.length()) {return 0;}
+    if (this.length() > other.length()) {return 1;}
+    return -1;
+  }
+
 }

3741f287eb8fcdcaef8f714ffb86e77a6e5b651e
Kevin Chen <kchen01@stuy.edu>
2018-11-25 22:45:57 -0500

    added more stuff

--- MyString.java
+++ MyString.java
@@ -3,15 +3,22 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
   private char[] data;
   String arrayerror = "Will throw Excpetion if start or end are negative, if end is greater than length(), or if start is greater than end";
   public MyString(CharSequence s){
+    if (s.length() < 0) {
+      throw new StringIndexOutOfBoundsException("invalid parameters");
+}
+    data = new char[s.length()];
+    for (int i = 0; i < s.length(); i += 1) {
+      data[i] = s.charAt(i);
+    }
   }
-  public CharSequence subSequence(int start, int end) {
-    if (start < 0 || end < 0 || end > data.length) {throw new IndexOutOfBoundsException(arrayerror);}
+  public MyString subSequence(int start, int end) {
+    if (start < 0 || end < 0 || end > data.length || start > end) {throw new StringIndexOutOfBoundsException(arrayerror);}
     CharSequence input = this.subSequence(start, end);
     MyString output = new MyString(input);
     return output;
   }
   public char charAt(int index) {
-    if (index < 0 || index > data.length) {throw new IndexOutOfBoundsException(arrayerror);}
+    if (index < 0 || index >= data.length) {throw new StringIndexOutOfBoundsException(arrayerror);}
     return data[index];
   }
 

c06e2be40209e38d2fb66f98c87b2f42c01f6531
Kevin Chen <kchen01@stuy.edu>
2018-11-26 22:04:34 -0500

    fixed Mystring

--- MyString.java
+++ MyString.java
@@ -23,23 +23,27 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
     data = new char[length];
   }
   public CharSequence subSequence(int start, int end) {
+    // will throw error if  if beginIndex or endIndex are negative, if endIndex is greater than length(), or if beginIndex is greater than startIndex
     if (start < 0 || end < 0 || end > data.length || start > end) {throw new IndexOutOfBoundsException(arrayerror);}
     MyString output = new MyString(end - start);
+    // counter to make it eaiser
     int count = 0;
     for (int index = start; index < end; index += 1) {
       output.data[count] = data[index];
       count++;
     }
+    // a mystring is a CharSequence
     return output;
   }
   public char charAt(int index) {
-    if (index < 0 || index >= data.length) {throw new StringIndexOutOfBoundsException(arrayerror);}
+    if (index < 0 || index >= data.length) {throw new IndexOutOfBoundsException(arrayerror);}
     return data[index];
   }
-
+  // returns data.length beccause data is a private variable
   public int length() {
     return data.length;
   }
+  // compare to compares the value of the characters of the string until there are no characters left in one of them. Then checks if the lengths differ.
   public int compareTo(CharSequence other) {
     for (int i = 0; i < this.length() && i < other.length(); i += 1) {
       if (this.charAt(i) < other.charAt(i)) {
@@ -53,7 +57,7 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
     if (this.length() > other.length()) {return 1;}
     return -1;
   }
-
+// quick toString using a loop
   public String toString() {
     String output = "";
     for (int i = 0; i < length(); i += 1) {
